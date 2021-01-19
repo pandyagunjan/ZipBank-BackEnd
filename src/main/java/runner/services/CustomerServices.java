2b27c5b134dd5b8d88295d39f6c46f524c131a39
ZekaiHe <51280445+ZekaiHe@users.noreply.github.com>
2021-01-19 13:54:23 -0500

    Update CustomerServices.java
    added logger for debugging heroku

--- src/main/java/runner/services/CustomerServices.java
+++ src/main/java/runner/services/CustomerServices.java
@@ -57,7 +57,9 @@ public class CustomerServices {
 
     //Check if the username already exist
     public Boolean checkLogin(Login login) {
+        loggerService.log(Level.INFO, "I am in first line of checkLogin");
         List<String> logins= customerRepo.findAllLoginsNative();
+        loggerService.log(Level.INFO, "I am have passed customerRepo.findAllLoginsNative");
         long count = logins.stream().filter(name -> name.equalsIgnoreCase(login.getUsername())).count();
         return count!=0 ? true:false;
     }
